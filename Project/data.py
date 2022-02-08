c977869d0380cd35b17da7fcbee0c1c5d49aa7fb
Worachet18 <worachetpethaisong543@gmail.com>
2022-02-08 10:50:56 +0700

    aa

---
+++ Project/data.py
@@ -0,0 +1,3 @@
+import random
+randtemp = random.randint(25,28)
+print(randtemp)
--- Project/test.py
+++ Project/test.py
@@ -1,6 +1,49 @@
-from datetime import*
-valdays = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-dates =[datetime.now().date()-timedelta(days=i)
-                             for i in  valdays]
+from tracemalloc import stop
+import pandas as pd 
+import csv
+import os
+from datetime import date
+from datetime import datetime
+from datetime import time
+import numpy as np
+import random
 
-print(len(dates))
+minute=00
+hour=00
+days=7
+mount=1
+year=2022
+randtemp = random.randint(25,28)
+randhumid = random.randint(60,80)
+
+try:
+    os.makedirs("c:/poject/test")
+except FileExistsError:
+    pass  
+
+# day,mount,year,hour,minute,secone,temp,humid
+
+os.chdir("c:/poject/test")
+with open('testpjinno.csv','a',newline="") as f:
+    mv = csv.writer(f)
+    # mv.writerow(['day','mount','year','hour','minute','secone','temp','humid'])
+    
+    while mount<3 :
+        days = 1
+        while days<31 :
+            hour = 0
+            while hour<24 :
+                minute = 0
+                while minute<60 :
+                    for i in range(1):
+                        mv.writerow([days,mount,year,hour,minute,00,random.randint(25,28),random.randint(60,80)])
+                        mv.writerow([days,mount,year,hour,minute,30,random.randint(25,28),random.randint(60,80)])
+                    minute = minute+1
+                hour = hour+1
+            days = days+1
+        mount = mount+1
+        
+        
+        
+
+
